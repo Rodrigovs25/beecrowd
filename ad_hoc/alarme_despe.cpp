8a1cc6d7e17ab21140cd3acd29858394cd5ea5e9
Rodrigovs25 <rodrigo2002.rvs@gmail.com>
2024-10-02 15:18:09 -0300

    1103-atua

--- ad_hoc/alarme_despe.cpp
+++ ad_hoc/alarme_despe.cpp
@@ -19,19 +19,13 @@ int main(int argc, char const *argv[])
                 minutos = (h2-h1)*60 - (m1-m2);
             }
         } else if (h1 == h2) {
-            if(m1 < m2){
+            if(m1 <= m2){
                 minutos = m2-m1;
             } else if(m1 > m2) {
                 minutos = 24*60 - (m1-m2);
-            } else {
-                minutos = 0;
             }
         } else {
-            if(m1 <= m2){
-                minutos = (23-(h1-h2))*60 + (60-m1+m2);
-            } else {
-                minutos = (23-(h1-h2))*60 + (60-m1+m2);
-            }
+            minutos = (23-(h1-h2))*60 + (60-m1+m2);
         }
         cout << minutos << endl;
     }
